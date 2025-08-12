e5fa1f6aeddaca1fdcb80bc8e78c892ed494e37f
cd <cdhj996@gmail.com>
2025-08-12 05:58:17 +0000

    refactor(utils): remove unused getMimeType function and simplify blob creation
    - Remove getMimeType function as it always returned a static MIME type
    - Update streamDecryptWithPassword and streamDecryptWithPrivateKey to use a default MIME type when creating Blobs

--- packages/utils/src/crypto/index.ts
+++ packages/utils/src/crypto/index.ts
@@ -289,10 +289,6 @@ async function hashFile(file: File): Promise<Uint8Array> {
   }
 }
 
-function getMimeType(ext: string): string {
-  return 'application/octet-stream'
-}
-
 // Create streaming header
 function createStreamHeader(streamHeader: StreamHeader) {
   const header: HeaderData = {
@@ -556,7 +552,7 @@ export async function streamDecryptWithPassword(options: StreamDecryptOptions) {
     cipher.destroy()
     onStage?.('Creating decrypted file...')
 
-    return { file: new Blob(chunks as BlobPart[], { type: getMimeType(header.e) }), signatureValid: undefined }
+    return { file: new Blob(chunks as BlobPart[]), signatureValid: undefined }
   } finally {
     if (key) {
       secureClear(key.buffer)
@@ -691,7 +687,7 @@ export async function streamDecryptWithPrivateKey(options: StreamDecryptOptions)
     cipher.destroy()
     onStage?.('Creating decrypted file...')
 
-    return { file: new Blob(chunks as BlobPart[], { type: getMimeType(header.e) }), signatureValid: isValid }
+    return { file: new Blob(chunks as BlobPart[]), signatureValid: isValid }
   } finally {
     if (key) {
       secureClear(key.buffer)
